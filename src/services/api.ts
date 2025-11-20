36b61ee0247d3acc0092677071246f9eabfbc83b
phemdev2 <135333734+phemdev2@users.noreply.github.com>
2025-11-20 15:34:03 +0100

    Change API base URL to production server

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE = "http://127.0.0.1:8000/api";
+const API_BASE = "https://saloon.printziontechnology.com.ng/api";
 
 // ✅ Fetch all products
 export const fetchProducts = async () => {
